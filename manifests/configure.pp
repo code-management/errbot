43b193149ab08c920702e64ecf610f9e891b8e4b
David Hayes <david.hayes@s3group.com>
2016-03-15 13:57:21 +0000

    Add configuration class

---
+++ manifests/configure.pp
@@ -0,0 +1,18 @@
+# Class: errbot::configure
+# ===========================
+#
+# Class for configuring errbot instance via config.py
+#
+# Parameters
+# ----------
+#
+class errbot::configure (
+
+) {
+
+  file { "${::errbot::virtualenv_dir}/config.py":
+    ensure  => 'present',
+    content => template('errbot/config.py.erb'),
+    owner   => $::errbot::user,
+  }
+}
