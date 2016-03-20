0eec302e2fbaf1d95aac3db9669282dc2f470da6
David Hayes <david.hayes@s3group.com>
2016-03-20 16:16:57 +0000

    Add class to write config.py using config_file param

---
+++ manifests/config/conf_file.pp
@@ -0,0 +1,20 @@
+# Class: errbot::config::conf_file
+# ===========================
+#
+# Class writes errbot's config.py file from a user
+# specified file resource.
+#
+# Should be considered private and not used directly.
+#
+# Parameters
+# ----------
+#
+class errbot::config::conf_file (
+) {
+
+  file { "${::errbot::virtualenv_dir}/config.py":
+    ensure => 'present',
+    source => $::errbot::config_file,
+    owner  => $::errbot::bot_user,
+  }
+}
